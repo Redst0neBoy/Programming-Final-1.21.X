812321ebe265d2f0414fcfea24b2697feacbed33
Redst0neBoy <andymartinezmendez1110@gmail.com>
2025-05-12 21:27:52 -0600

    AFTER AN HOUR I SET THIS UP!

--- src/main/java/net/redst0ne/programmingfinal/ProgrammingFinal.java
+++ src/main/java/net/redst0ne/programmingfinal/ProgrammingFinal.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// yo ts cool
 public class ProgrammingFinal implements ModInitializer {
 	public static final String MOD_ID = "programming-final";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
