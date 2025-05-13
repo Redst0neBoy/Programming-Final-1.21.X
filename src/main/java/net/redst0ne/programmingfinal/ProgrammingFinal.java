60f9e50aa5b7598c307e00ed50523b357916e1e2
Redst0neBoy <andymartinezmendez1110@gmail.com>
2025-05-12 21:21:53 -0600

    yo

---
+++ src/main/java/net/redst0ne/programmingfinal/ProgrammingFinal.java
@@ -0,0 +1,15 @@
+package net.redst0ne.programmingfinal;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProgrammingFinal implements ModInitializer {
+	public static final String MOD_ID = "programming-final";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/redst0ne/programmingfinal/ProgrammingFinalClient.java
@@ -0,0 +1,10 @@
+package net.redst0ne.programmingfinal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProgrammingFinalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
