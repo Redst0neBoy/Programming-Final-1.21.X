4589483e0473caaef40b1042840188c6b05cb052
Redst0neBoy <andymartinezmendez1110@gmail.com>
2025-05-13 22:57:29 -0600

    AFTER AN HOUR I SET THIS UP!

--- src/main/java/net/redst0ne/programmingfinal/ProgrammingFinal.java
+++ src/main/java/net/redst0ne/programmingfinal/ProgrammingFinal.java
@@ -2,6 +2,7 @@ package net.redst0ne.programmingfinal;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.redst0ne.programmingfinal.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,6 @@ public class ProgrammingFinal implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/redst0ne/programmingfinal/item/ModItems.java
@@ -0,0 +1,34 @@
+package net.redst0ne.programmingfinal.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.redst0ne.programmingfinal.ProgrammingFinal;
+
+public class ModItems {
+
+
+    public static final Item BLUE_APPLE = registerItem("blue_apple", new Item(new Item.Settings()));
+
+    public static final Item PREPARED_ENDSTONE = registerItem("prepared_endstone", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ProgrammingFinal.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems() {
+        ProgrammingFinal.LOGGER.info("Registering Mod Items for " + ProgrammingFinal.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(BLUE_APPLE);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PREPARED_ENDSTONE);
+        });
+    }
+}
